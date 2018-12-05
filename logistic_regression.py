1259306100b89cd1c6eabb92ca18416805a1d70a
cuixianming <cuixianming@gotokeep.com>
2018-12-05 16:11:52 +0800

    同步进度

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,9 +1,11 @@
 import os
 
-from sklearn import datasets
+from sklearn import datasets, preprocessing
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.neighbors import NearestNeighbors
 
 DATA_ROOT = '/Users/xana/Dev/data/aclImdb'
 VOCAB_NAME = 'imdb.vocab'
@@ -42,16 +44,29 @@ def classifier_report(classifier, train_data, test_data, vocab):
     print(classification_report(test_data.target, predict, target_names=train_data.target_names))
 
 
+def classifier_report2(classifier, train_data, test_data, vocab):
+    x_train, x_test = tokenized(train_data, test_data, vocab)
+    x_train_normalized = preprocessing.normalize(x_train)
+    x_test_normalized = preprocessing.normalize(x_test)
+    classifier.fit(x_train_normalized, train_data.target)
+    predict = classifier.predict(x_test_normalized)
+    print(classification_report(test_data.target, predict, target_names=train_data.target_names))
+
+
 if __name__ == '__main__':
     vocab = get_vocab(os.path.join(DATA_ROOT, VOCAB_NAME))
     train_data = load_data_set(os.path.join(DATA_ROOT, TRAIN_PATH), 'Train')
     test_data = load_data_set(os.path.join(DATA_ROOT, TEST_PATH), 'Test')
 
-    solvers = ('lbfgs', 'newton-cg', 'sag')
-    for s in solvers:
-        print(s)
-        logistic_regression = LogisticRegression(penalty='l2', dual=False, tol=0.0001, fit_intercept=True,
-                                                 intercept_scaling=1, class_weight='balanced', random_state=None,
-                                                 solver=s, max_iter=3000, multi_class='ovr', verbose=0,
-                                                 warm_start=False, n_jobs=-1)
-        classifier_report(logistic_regression, train_data, test_data, vocab)
+    solvers = 'lbfgs'
+    print(solvers)
+    # logistic_regression = LogisticRegression(penalty='l2', dual=False, tol=0.0001, fit_intercept=True,
+    #                                          intercept_scaling=1, class_weight='balanced', random_state=None,
+    #                                          solver=solvers, max_iter=3000, multi_class='ovr', verbose=0,
+    #                                          warm_start=False, n_jobs=-1)
+    nbc = MultinomialNB()
+    knn = NearestNeighbors(2)
+    # classifier_report(logistic_regression, train_data, test_data, vocab)
+    classifier_report(nbc, train_data, test_data, vocab)
+    classifier_report(knn, train_data, test_data, vocab)
+    # classifier_report2(logistic_regression, train_data, test_data, vocab)
